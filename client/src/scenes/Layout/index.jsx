45c4859498092eb99a913da507eeff4a1730e169
Cerqueira <contatocerq@gmail.com>
2023-02-28 11:32:17 -0300

    Sidebar

--- client/src/scenes/Layout/index.jsx
+++ client/src/scenes/Layout/index.jsx
@@ -3,12 +3,22 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
 
 function Layout() {
+  const isNonMobileScreens = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNonMobileScreens ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+        isNonMobileScreens={isNonMobileScreens}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
         <Outlet />
       </Box>
     </Box>
