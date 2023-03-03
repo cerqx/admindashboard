9ecfc9bb77f25ea9053b9ee5da12c955a97def38
Cerqueira <contatocerq@gmail.com>
2023-03-03 14:29:50 -0300

    Products

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,13 +4,17 @@ export const api = createApi({
   //baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
   reducerPath: "adminApi",
-  tagTypes: ["User"],
+  tagTypes: ["User", "Products"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       providesTags: ["User"],
     }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
   }),
 });
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
