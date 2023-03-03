ad85cbf99032bd49b48254c9156062d9f0500336
Cerqueira <contatocerq@gmail.com>
2023-03-03 15:31:00 -0300

    Product component

--- client/src/scenes/Products/index.jsx
+++ client/src/scenes/Products/index.jsx
@@ -1,71 +1,8 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  Collapse,
-  Button,
-  Typography,
-  Rating,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
+import React from "react";
+import { Box, useMediaQuery } from "@mui/material";
 import { useGetProductsQuery } from "state/api";
 import Header from "components/Header";
-
-const Product = ({ _id, name, description, price, rating, category, supply, stat }) => {
-  const theme = useTheme();
-  const [isExpanded, setIsExpended] = useState(false);
-
-  return (
-    <Card
-      sx={{
-        backgroundImage: "none",
-        backgroundColor: theme.palette.background.alt,
-        borderRadius: "0.55rem",
-      }}
-    >
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color={theme.palette.secondary[700]}>
-          {category}
-        </Typography>
-
-        <Typography variant="h5" component="div">
-          {name}
-        </Typography>
-
-        <Typography sx={{ mb: "1.5rem" }} color={theme.palette.secondary[400]}>
-          ${Number(price).toFixed(2)}
-        </Typography>
-        <Rating value={rating} readOnly />
-
-        <Typography variant="body2">{description}</Typography>
-      </CardContent>
-
-      <CardActions>
-        <Button variant="primary" size="small" onClick={() => setIsExpended(!isExpanded)}>
-          See More
-        </Button>
-      </CardActions>
-      <Collapse
-        in={isExpanded}
-        timeout="auto"
-        unmountOnExit
-        sx={{
-          color: theme.palette.neutral[300],
-        }}
-      >
-        <CardContent>
-          <Typography>id: {_id}</Typography>
-          <Typography>Supply Left: {supply}</Typography>
-          <Typography>Yearly Sales This Year: {stat.yearlySalesTotal}</Typography>
-          <Typography>Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-  );
-};
+import Product from "components/Product";
 
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
@@ -88,7 +25,7 @@ const Products = () => {
             },
           }}
         >
-          {data.map(({ _id, name, description, price, rating, category, supply, stat }) => (
+          {data?.map(({ _id, name, description, price, rating, category, supply, stat }) => (
             <Product
               key={_id}
               _id={_id}
