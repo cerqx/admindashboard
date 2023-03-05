696c0ac86822018eca08365e2d195271679692b0
Cerqueira <contatocerq@gmail.com>
2023-03-05 20:33:02 -0300

    Done

--- client/src/scenes/Customers/index.jsx
+++ client/src/scenes/Customers/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetCustomersQuery } from "state/api";
+import Header from "components/Header";
+import { DataGrid } from "@mui/x-data-grid";
 
 function Customers() {
+  const theme = useTheme();
+  const { data, isLoading } = useGetCustomersQuery();
+  console.log({ data });
+
   return <div>Customers</div>;
 }
 
