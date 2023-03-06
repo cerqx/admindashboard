1ffc75f95970b15e7a42a6110bbd94d9b726a96c
Cerqueira <contatocerq@gmail.com>
2023-03-05 21:54:54 -0300

    Done

--- client/src/scenes/Customers/index.jsx
+++ client/src/scenes/Customers/index.jsx
@@ -30,7 +30,7 @@ function Customers() {
       headerName: "Phone Number",
       flex: 0.5,
       renderCell: (params) => {
-        return params?.value?.replace(/^(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
+        return params?.value?.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
       },
     },
     {
