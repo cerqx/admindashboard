c9b8520d8ecc5ff3031ab74e4049dc87814a22b4
Cerqueira <contatocerq@gmail.com>
2023-03-05 21:47:51 -0300

    Done

--- client/src/scenes/Customers/index.jsx
+++ client/src/scenes/Customers/index.jsx
@@ -9,9 +9,53 @@ function Customers() {
   const { data, isLoading } = useGetCustomersQuery();
   console.log({ data });
 
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params?.value?.replace(/^(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="CUSTOMERS" subtitle="List of Customers" />
+      <Box mt="40px" height="75vh">
+        <DataGrid rows={data || []} columns={columns} loading={isLoading || !data} getRowId={(row) => row._id} />
+      </Box>
     </Box>
   );
 }
