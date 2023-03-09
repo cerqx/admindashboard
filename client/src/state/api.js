90842317ff79e07baa4af56ef2fcd963eb16583d
Cerqueira <contatocerq@gmail.com>
2023-03-09 00:43:42 -0300

    getSales query done

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,7 +4,7 @@ export const api = createApi({
   //baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001" }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography"],
+  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales "],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -30,6 +30,10 @@ export const api = createApi({
       query: () => "client/geography",
       providesTags: ["Geography"],
     }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
   }),
 });
 
@@ -39,4 +43,5 @@ export const {
   useGetCustomersQuery,
   useGetTransactionsQuery,
   useGetGeographyQuery,
+  useGetSalesQuery,
 } = api;
