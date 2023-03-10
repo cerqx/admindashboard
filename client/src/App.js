2f95b2a05e542fec52c05b5c0e2ff2b9bb31b08e
Cerqueira <contatocerq@gmail.com>
2023-03-10 11:43:48 -0300

    Monthly chart beginning

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Transactions from "scenes/Transactions";
 import Geography from "scenes/Geography";
 import Overview from "scenes/Overview";
 import Daily from "scenes/Daily";
+import Monthly from "scenes/Monthly";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -31,6 +32,7 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
