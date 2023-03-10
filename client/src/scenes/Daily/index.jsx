8472f42b0414b8354930611de1c2562e79bb0b86
Cerqueira <contatocerq@gmail.com>
2023-03-10 00:09:04 -0300

    done

--- client/src/scenes/Daily/index.jsx
+++ client/src/scenes/Daily/index.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { Box, useTheme } from "@mui/material";
+import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
+import { useGetSalesQuery } from "state/api";
 
 const Daily = () => {
   return <div>Daily</div>;
