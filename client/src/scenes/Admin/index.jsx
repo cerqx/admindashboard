b810ee37a6b927482c3c688cfa675442401c5788
Cerqueira <contatocerq@gmail.com>
2023-03-11 22:09:23 -0300

    Done

--- client/src/scenes/Admin/index.jsx
+++ client/src/scenes/Admin/index.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetAdminsQuery } from "state/api";
+import { DataGrid } from "@mui/x-data-grid";
+import Header from "components/Header";
 
 const Admin = () => {
+  const { data, isLoading } = useGetAdminsQuery();
+  const theme = useTheme();
+
+  console.log(data);
+
   return <div>Admin</div>;
 };
 
