d4f18219da2922889448f65fb7cd125c0185c134
Cerqueira <contatocerq@gmail.com>
2023-03-12 00:20:46 -0300

    getUserPerformance beginning

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getAdmins = async (req, res) => {
   try {
     const admins = await User.find({ role: "admin" }).select("-password");
     res.status(200).json(admins);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserPerformance = async (req, res) => {
+  try {
+    const { id } = req.params;
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
