32798213a00435d2d05a92c657b0421dab5262e1
Cerqueira <contatocerq@gmail.com>
2023-03-11 21:55:47 -0300

    getAdmins done

---
+++ server/controllers/management.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import User from "../models/User.js";
+
+export const getAdmins = async (req, res) => {
+  try {
+    const admins = User.find({ role: "admin" }).select("-password");
+    res.status(200).json(admins);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
